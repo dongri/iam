8666930c669dd4cf40a64691be3084a4d5a9e3f6
Dongri Jin <dongrify@gmail.com>
2019-04-17 15:07:14 +0900

    HTML

--- web/Main.hs
+++ web/Main.hs
@@ -9,13 +9,15 @@ import Text.Blaze.Html.Renderer.Text (renderHtml)
 import Data.Monoid (mconcat)
 import Control.Monad (liftM)
 
+-- topWidget = $(hamletFile "views/top-widget.hamlet")
+-- bottomWidget = $(hamletFile "views/bottom-widget.hamlet")
+
 main :: IO ()
 main = do
   port <- liftM read $ getEnv "PORT"
   scotty port $ do
-
     get "/" $ do
-      let title = pack "D"
+      -- let title = pack "D"
       html $ renderHtml $ $(hamletFile "views/index.hamlet") undefined
 
     get "/hello" $ do
